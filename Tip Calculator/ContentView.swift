fdf605d8225ce8ada2c477987fcd6875df39dba2
pserb <paulserbanescu3@gmail.com>
2021-11-07 14:12:23 -0500

    .gitignore is now working

--- Tip Calculator/ContentView.swift
+++ Tip Calculator/ContentView.swift
@@ -8,9 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    @FocusState private var isFocused : Bool
+    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    var currency: FloatingPointFormatStyle<Double>.Currency {
+        return .currency(code: Locale.current.currencyCode ?? "USD")
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: currency)
+                        .keyboardType(.decimalPad)
+                        .focused($isFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }
+                } header: {
+                    Text("Bill amount")
+                }
+                
+                Section {
+                    Picker ("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                        .pickerStyle(.segmented)
+                    }
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: currency)
+                } header: {
+                    Text("payment per person")
+                }
+                
+                Section {
+                    Text(totalPerPerson * Double(numberOfPeople + 2), format: currency)
+                } header: {
+                    Text("Total bill")
+                }
+            }
+            .navigationTitle("Tip Calculator")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        isFocused = false
+                    }
+                }
+            }
+        }
     }
 }
 
